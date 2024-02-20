83a531eb78f22a4f0d69687a1e1169a3ba041a00
LinkkStabs <160384195+linkkstabs@users.noreply.github.com>
2024-02-20 21:45:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ keepAlive();
 function formatTime() { //Credits to himika#0001 and never#0001
   const date = new Date();
   const options = {
-    timeZone: 'Japan', //https://www.zeitverschiebung.net/en/ and find your city and enter here
+    timeZone: '', //https://www.zeitverschiebung.net/en/ and find your city and enter here
     hour12: true,
     hour: 'numeric',
     minute: 'numeric'
