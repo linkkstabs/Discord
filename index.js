a9471e54bb0b12f48d6b3345069fa5c1010feb34
LinkkStabs <160384195+linkkstabs@users.noreply.github.com>
2024-04-15 17:33:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ client.on('ready', async () => {
     .setName('Jiji')
     .setDetails(`Delivering.. [${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://media.discordapp.net/attachments/1181461047736029206/1229400101278060665/16_7GMbPZgZXhUMHdsfFfkQ2x.png?ex=662f8b11&is=661d1611&hm=83f7dc6080a0fe001230a0b16eb93e3c0283508db6885f649224c4c62aa86a06&=&format=webp&quality=lossless&width=687&height=366') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Kiki') //Text when you hover the Large image
-    .setAssetsSmallImage('https://media.discordapp.net/attachments/1181461047736029206/1229400473056837672/AATXAJxwXC-2Py2nBHFJ7l-oB93QeSjqa6YePLXL_ws900-c-k-c0xffffffff-no-rj-mo.png?ex=662f8b69&is=661d1669&hm=a33db242078473d1bd7ea7f068e64c5e782f569fa3583c717917c51f19f1043f&=&format=webp&quality=lossless&width=437&height=437') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Jiji') //Text when you hover the Small image
+ .setAssetsLargeImage('https://media.discordapp.net/attachments/1181461047736029206/1229400473056837672/AATXAJxwXC-2Py2nBHFJ7l-oB93QeSjqa6YePLXL_ws900-c-k-c0xffffffff-no-rj-mo.png?ex=662f8b69&is=661d1669&hm=a33db242078473d1bd7ea7f068e64c5e782f569fa3583c717917c51f19f1043f&=&format=webp&quality=lossless&width=437&height=437') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('Jiji') //Text when you hover the Large image
+    .setAssetsSmallImage('https://media.discordapp.net/attachments/1181461047736029206/1229400101278060665/16_7GMbPZgZXhUMHdsfFfkQ2x.png?ex=662f8b11&is=661d1611&hm=83f7dc6080a0fe001230a0b16eb93e3c0283508db6885f649224c4c62aa86a06&=&format=webp&quality=lossless&width=687&height=366') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('Kiki') //Text when you hover the Small image
     
 
   client.user.setActivity(r);
@@ -43,7 +43,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = `となりのトトロ [${newTime}]`;
+      const newDetails = `Delivering..[${newTime}]`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
