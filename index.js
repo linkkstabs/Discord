ec59b298f24f5b392c81a86293bf138adf4c693c
LinkkStabs <160384195+linkkstabs@users.noreply.github.com>
2024-04-15 17:29:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ keepAlive();
 function formatTime() { //Credits to himika#0001 and never#0001
   const date = new Date();
   const options = {
-    timeZone: 'Asia/Kolkata', //https://www.zeitverschiebung.net/en/ and find your city and enter here
+    timeZone: 'Japan', //https://www.zeitverschiebung.net/en/ and find your city and enter here
     hour12: true,
     hour: 'numeric',
     minute: 'numeric'
@@ -26,15 +26,15 @@ client.on('ready', async () => {
     .setApplicationId('1208472205432848406')
     .setType('STREAMING')
     .setURL('https://twitch.tv/linkkstabs') //Must be a youtube video link 
-    .setState('dreaming.. ')
-    .setName('Productive :>')
-    .setDetails(`Idle [${formatTime()}]`)
+    .setState("Kiki's Delivery Service")
+    .setName('Jiji')
+    .setDetails(`Delivering.. [${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://media.discordapp.net/attachments/1181126458744242206/1209548911115636776/giphy.gif?ex=65e75334&is=65d4de34&hm=1b0dd53d29217a98dcf6415e28ca93c80b47f93f6a39d1168640984010c97a01&=') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Totoro') //Text when you hover the Large image
-    .setAssetsSmallImage('https://media.discordapp.net/attachments/1181126458744242206/1209548911786594345/giphy_1.gif?ex=65e75334&is=65d4de34&hm=4a1616086a89747daaa343271ada6d04f39dfddde3a1f631d7402e87e3c9aed7&=') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Mei') //Text when you hover the Small image
-    .addButton('Carrd', 'https://linkkstabs.carrd.co')
+ .setAssetsLargeImage('https://media.discordapp.net/attachments/1181461047736029206/1229400101278060665/16_7GMbPZgZXhUMHdsfFfkQ2x.png?ex=662f8b11&is=661d1611&hm=83f7dc6080a0fe001230a0b16eb93e3c0283508db6885f649224c4c62aa86a06&=&format=webp&quality=lossless&width=687&height=366') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('Kiki') //Text when you hover the Large image
+    .setAssetsSmallImage('https://media.discordapp.net/attachments/1181461047736029206/1229400473056837672/AATXAJxwXC-2Py2nBHFJ7l-oB93QeSjqa6YePLXL_ws900-c-k-c0xffffffff-no-rj-mo.png?ex=662f8b69&is=661d1669&hm=a33db242078473d1bd7ea7f068e64c5e782f569fa3583c717917c51f19f1043f&=&format=webp&quality=lossless&width=437&height=437') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('Jiji') //Text when you hover the Small image
+    
 
   client.user.setActivity(r);
   client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
